3edf60071fc1d60f4b41c48cad3098cfda70d090
fridaeira <fridaasby1@hotmail.com>
2024-02-28 21:41:21 +0100

    Småfix

---
+++ my-app/src/components/header.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+
+export default function Appheader() {
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">Daun tattoo</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Hem</Nav.Link>
+            <Nav.Link href="#link">Galleri</Nav.Link>
+            <Nav.Link href="#link">Skötselråd</Nav.Link>
+            <Nav.Link href="#link">Vanliga frågor</Nav.Link>
+            <Nav.Link href="#link">Merch</Nav.Link>
+            <Nav.Link href="#link">Boka tid</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
---
+++ my-app/src/components/hero.js
@@ -0,0 +1,6 @@
+import React from "react";
+import Carousel from "react-bootstrap/Carousel";
+
+export default function Apphero() {
+  return <p>hello</p>;
+}
