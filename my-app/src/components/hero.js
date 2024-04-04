4ae3b69a8e8ecece317b09a1494005c6e1f347e9
fridaeira <fridaasby1@hotmail.com>
2024-04-04 21:24:03 +0200

    Hero klar
    Tagit bort onödig css-kod plus fixat klart heron

--- my-app/src/components/hero.js
+++ my-app/src/components/hero.js
@@ -2,22 +2,9 @@ import React from "react";
 
 export default function Apphero() {
   return (
-    <div
-      className="d-flex align-items-end justify-content-end p-5"
-      style={{
-        backgroundImage: "url(/philip.jpg)",
-        maxWidth: "100%",
-        maxHeight: "100%",
-      }}
-    >
+    <div className="hero-image">
       <div>
-        <h2 style={{ textAlign: "center" }}>Redo för nästa semester?</h2>
-        <h2 style={{ textAlign: "center" }}>
-          Vi har över 2000 hotell som väntar på dig!
-        </h2>
-        <div className="text-center">
-          <button>Boka nu!</button>
-        </div>
+        <button className="button">Boka</button>
       </div>
     </div>
   );
